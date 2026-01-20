cc020e5d9d03aa5d721b2481b1305ec13fddafcd
Gorakh Gonda <gorak4u@gmail.com>
2026-01-20 06:35:25 +0000

    Parameter minute failed on Cron[scheduled_puppet_run]: Munging failed fo

--- src/lib/puppet/cassandra_pfpt/manifests/puppet.ts
+++ src/lib/puppet/cassandra_pfpt/manifests/puppet.ts
@@ -5,22 +5,35 @@ class cassandra_pfpt::puppet inherits cassandra_pfpt {
   # Stagger the cron job across the hour to avoid all nodes running at once.
   $cron_minute_1 = fqdn_rand(30)
   $cron_minute_2 = $cron_minute_1 + 30
-  
-  # Default schedule: runs twice an hour, staggered.
-  $default_schedule = "$\\{cron_minute_1},$\\{cron_minute_2} * * * *"
-  
-  # Use the schedule from the parameter if provided, otherwise use the staggered default.
-  $final_schedule = pick($\\
-{puppet_cron_schedule}, $default_schedule)
+
+  # Check if a custom schedule is provided via Hiera.
+  if $\\{puppet_cron_schedule} {
+    # If a custom schedule is defined, parse it.
+    $schedule_parts = split($\\{puppet_cron_schedule}, ' ')
+    $minute         = $schedule_parts[0]
+    $hour           = $schedule_parts[1]
+    $monthday       = $schedule_parts[2]
+    $month          = $schedule_parts[3]
+    $weekday        = $schedule_parts[4]
+  } else {
+    # If no custom schedule, use the default staggered schedule.
+    # The minute parameter accepts an array of values.
+    $minute   = [$\\
+{cron_minute_1}, $\\{cron_minute_2}]
+    $hour     = '*'
+    $monthday = '*'
+    $month    = '*'
+    $weekday  = '*'
+  }
 
   cron { 'scheduled_puppet_run':
     command  => '[ ! -f /var/lib/puppet-disabled ] && /opt/puppetlabs/bin/puppet agent -v --onetime',
     user     => 'root',
-    minute   => split($final_schedule, ' ')[0],
-    hour     => split($final_schedule, ' ')[1],
-    monthday => split($final_schedule, ' ')[2],
-    month    => split($final_schedule, ' ')[3],
-    weekday  => split($final_schedule, ' ')[4],
+    minute   => $minute,
+    hour     => $hour,
+    monthday => $monthday,
+    month    => $month,
+    weekday  => $weekday,
   }
 }
 `.trim();
