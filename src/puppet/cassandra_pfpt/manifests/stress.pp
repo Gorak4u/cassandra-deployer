5c6419cdf6a864f3ac94a09cc21f7c59a1505bc6
Gorakh Gonda <gorak4u@gmail.com>
2026-01-22 10:10:17 +0000

    use erb instead of epp.

--- src/puppet/cassandra_pfpt/manifests/stress.pp
+++ src/puppet/cassandra_pfpt/manifests/stress.pp
@@ -1,34 +1,23 @@
-# @summary Manages Cassandra stress testing tools and their configuration.
-class cassandra_pfpt::stress(
+class cassandra_pfpt::stress (
   String $user,
   Sensitive[String] $password,
   Boolean $ssl_enabled,
-  String $config_path = '/etc/cassandra/conf/stress.conf',
-){
-  package { 'cassandra-tools':
-    ensure => installed,
+) {
+  ensure_packages(['cassandra-tools'])
+
+  file { '/etc/cassandra/conf/stress.conf':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
+    content => template('cassandra_pfpt/stress.conf.erb'),
   }
 
   file { '/usr/local/bin/stress-test.sh':
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-    source  => 'puppet:///modules/cassandra_pfpt/stress-test.sh',
-    require => Package['cassandra-tools'],
-  }
-
-  # Create a secure configuration file for the stress test wrapper
-  file { $config_path:
-    ensure  => file,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0600', # Secure: only root can read
-    content => epp('cassandra_pfpt/stress.conf.epp', {
-      'user'     => $user,
-      'password' => $password.unwrap,
-      'ssl'      => $ssl_enabled,
-    }),
-    require => Package['cassandra-tools'],
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+    source => 'puppet:///modules/cassandra_pfpt/stress-test.sh',
   }
 }
